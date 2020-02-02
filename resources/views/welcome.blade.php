443733d951e801860498f1ad87a7bea72bd04ecc
Zwane Mdu <zwanemduduzi9@gmail.com>
2020-02-02 09:52:32 +0200

    mdu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -50,7 +50,7 @@
 
             .links > a {
                 color: #636b6f;
-                padding: 0 25px;
+                padding: 0 24px;
                 font-size: 13px;
                 font-weight: 600;
                 letter-spacing: .1rem;
